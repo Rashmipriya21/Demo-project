d08f2cc291837fad62dd772b856acf5ed64f9d04
Rashmi Priya <35029328+Rashmipriya21@users.noreply.github.com>
2023-11-10 16:05:47 +0530

    Update ec2.tf
    ami changed as per ohio region

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,6 @@ provider "aws" {
     region = "us-east-2"
 }
 resource "aws_instance" "myec2" {
-   ami = "ami-0fc682b2a42e57ca2"
+   ami = "ami-00744e52917f35c39"
    instance_type = "t2.micro"
  }
